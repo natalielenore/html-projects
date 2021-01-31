8630db266bdcf3d5f56cb0ba278e15d69ec4d5b7
Natalie Graham <nataliegraham@berkeley.edu>
2021-01-31 01:09:21 -0800

    updated values with slider ooo

--- interactive-pricing-component-main/app.js
+++ interactive-pricing-component-main/app.js
@@ -1,7 +1,45 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("pv-num");
-output.innerHTML = slider.value;
+var monies = document.getElementById("dollars");
+// output.innerHTML = slider.value;
 
 slider.oninput = function() {
-  output.innerHTML = "1234";
+	// do cases on value of this.value and then
+	// determine waht dollars and pv number should ouput.
+	// briallinat 
+
+	var cur = this.value;
+
+	switch (true) {
+
+		//       <!-- 
+  //     10K pageviews / $8 per month
+  //     50K pageviews / $12 per month
+  //     100K pageviews / $16 per month
+  //     500k pageviews / $24 per month
+  //     1M pageviews / $36 per month -->
+		case cur == 0:
+			output.innerHTML = "10K";
+			monies.innerHTML = "$8.00";
+			break;
+		case cur == 1:
+			output.innerHTML = "50K";
+			monies.innerHTML = "$12.00";
+			break;
+		case cur == 2:
+			output.innerHTML = "100K";
+			monies.innerHTML = "$16.00";
+			break;
+		case cur == 3:
+			output.innerHTML = "500K";
+			monies.innerHTML = "$24.00";
+			break;
+		case cur == 4:
+			output.innerHTML = "1M";
+			monies.innerHTML = "$36.00";
+			break;
+
+	}
+  // output.innerHTML = this.value;
 }
+
