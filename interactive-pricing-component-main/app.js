8c1dba9163f2cf921a9c9d93f4575d8781135875
Natalie Graham <nataliegraham@berkeley.edu>
2021-02-04 19:30:57 -0800

    condenced logic

--- interactive-pricing-component-main/app.js
+++ interactive-pricing-component-main/app.js
@@ -8,61 +8,42 @@ var checkbox = document.getElementById("check");
 
 function change_values(discount, cur) {
 
-	if (discount) {
 
 		switch (true) {
 		case cur < 100:
 			output.innerHTML = "10K";
-			monies.innerHTML = "$6.00";
+			monies.innerHTML = "$8.00";
+			dis = "$6.00";
 			break;
 		case cur < 200:
 			output.innerHTML = "50K";
-			monies.innerHTML = "$9.00";
+			monies.innerHTML = "$12.00";
+			dis = "$9.00";
 			break;
 		case cur < 300:
 			output.innerHTML = "100K";
-			monies.innerHTML = "$12.00";
+			monies.innerHTML = "$16.00";
+			dis = "$12.00";
 			break;
 		case cur < 400:
 			output.innerHTML = "500K";
-			monies.innerHTML = "$18.00";
+			monies.innerHTML = "$24.00";
+			dis = "$18.00";
 			break;
 		case cur == 400:
 			output.innerHTML = "1M";
-			monies.innerHTML = "$27.00";
+			monies.innerHTML = "$36.00";
+			dis = "$27.00";
 			break;
 		}
-	}
-
-	else {
-
-		switch (true) {
-
-			case cur < 100:
-				output.innerHTML = "10K";
-				monies.innerHTML = "$8.00";
-				break;
-			case cur < 200:
-				output.innerHTML = "50K";
-				monies.innerHTML = "$12.00";
-				break;
-			case cur < 300:
-				output.innerHTML = "100K";
-				monies.innerHTML = "$16.00";
-				break;
-			case cur < 400:
-				output.innerHTML = "500K";
-				monies.innerHTML = "$24.00";
-				break;
-			case cur == 400:
-				output.innerHTML = "1M";
-				monies.innerHTML = "$36.00";
-				break;
+		console.log(discount)
+		
+		if (discount) {
+			console.log("hi")
+			monies.innerHTML = dis;
 		}
-	}
 }
 
-	
 
 
 
