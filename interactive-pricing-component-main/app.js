08df781e43881f6bac956cd07848886bc6ed3009
Natalie Graham <nataliegraham@berkeley.edu>
2021-02-04 19:02:25 -0800

    js logic done

--- interactive-pricing-component-main/app.js
+++ interactive-pricing-component-main/app.js
@@ -2,115 +2,42 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("pv-num");
 var monies = document.getElementById("dollars");
 var checkbox = document.getElementById("check");
-var checked = false;
-
-// checkbox.addEventListener('change', function() {
-//   if (this.checked) {
-//     checked = true;
-//   } else {
-//     checked = false;
-//   }
-
-// 	slider.oninput = function() {
-// 		// do cases on value of this.value and then
-// 		// determine waht dollars and pv number should ouput.
-// 		// briallinat 
-
-// 		var cur = this.value;
-
-// 		if (!checked) {
-
-// 		switch (true) {
-
-// 			//       <!-- 
-// 	  //     10K pageviews / $8 per month
-// 	  //     50K pageviews / $12 per month
-// 	  //     100K pageviews / $16 per month
-// 	  //     500k pageviews / $24 per month
-// 	  //     1M pageviews / $36 per month -->
-// 			case cur == 0:
-// 				output.innerHTML = "10K";
-// 				monies.innerHTML = "$8.00";
-// 				break;
-// 			case cur == 1:
-// 				output.innerHTML = "50K";
-// 				monies.innerHTML = "$12.00";
-// 				break;
-// 			case cur == 2:
-// 				output.innerHTML = "100K";
-// 				monies.innerHTML = "$16.00";
-// 				break;
-// 			case cur == 3:
-// 				output.innerHTML = "500K";
-// 				monies.innerHTML = "$24.00";
-// 				break;
-// 			case cur == 4:
-// 				output.innerHTML = "1M";
-// 				monies.innerHTML = "$36.00";
-// 				break;
-
-// 		}
-// 		}
-
-// 		else {
-// 		switch (true) {
-
-// 			//       <!-- 
-// 	  //     10K pageviews / $8 per month
-// 	  //     50K pageviews / $12 per month
-// 	  //     100K pageviews / $16 per month
-// 	  //     500k pageviews / $24 per month
-// 	  //     1M pageviews / $36 per month -->
-// 			case cur == 0:
-// 				output.innerHTML = "10K";
-// 				monies.innerHTML = "$6.00";
-// 				break;
-// 			case cur == 1:
-// 				output.innerHTML = "50K";
-// 				monies.innerHTML = "$9.00";
-// 				break;
-// 			case cur == 2:
-// 				output.innerHTML = "100K";
-// 				monies.innerHTML = "$12.00";
-// 				break;
-// 			case cur == 3:
-// 				output.innerHTML = "500K";
-// 				monies.innerHTML = "$18.00";
-// 				break;
-// 			case cur == 4:
-// 				output.innerHTML = "1M";
-// 				monies.innerHTML = "$27.00";
-// 				break;
-
-// 		}
-// 	}
-//   // output.innerHTML = this.value;
-// }
 
 
 
 
-// });
+function change_values(discount, cur) {
 
+	if (discount) {
 
+		switch (true) {
+		case cur < 100:
+			output.innerHTML = "10K";
+			monies.innerHTML = "$6.00";
+			break;
+		case cur < 200:
+			output.innerHTML = "50K";
+			monies.innerHTML = "$9.00";
+			break;
+		case cur < 300:
+			output.innerHTML = "100K";
+			monies.innerHTML = "$12.00";
+			break;
+		case cur < 400:
+			output.innerHTML = "500K";
+			monies.innerHTML = "$18.00";
+			break;
+		case cur == 400:
+			output.innerHTML = "1M";
+			monies.innerHTML = "$27.00";
+			break;
+		}
+	}
 
-
-slider.oninput = function() {
-	// do cases on value of this.value and then
-	// determine waht dollars and pv number should ouput.
-	// briallinat 
-	var cur = this.value;
-
-	if (!checkbox.checked) {
+	else {
 
 		switch (true) {
 
-			//       <!-- 
-	  //     10K pageviews / $8 per month
-	  //     50K pageviews / $12 per month
-	  //     100K pageviews / $16 per month
-	  //     500k pageviews / $24 per month
-	  //     1M pageviews / $36 per month -->
 			case cur < 100:
 				output.innerHTML = "10K";
 				monies.innerHTML = "$8.00";
@@ -131,52 +58,29 @@ slider.oninput = function() {
 				output.innerHTML = "1M";
 				monies.innerHTML = "$36.00";
 				break;
-
 		}
 	}
+}
 
-		else {
-		switch (true) {
+	
 
-			//       <!-- 
-	  //     10K pageviews / $8 per month
-	  //     50K pageviews / $12 per month
-	  //     100K pageviews / $16 per month
-	  //     500k pageviews / $24 per month
-	  //     1M pageviews / $36 per month -->
-			case cur == 0:
-				output.innerHTML = "10K";
-				monies.innerHTML = "$6.00";
-				break;
-			case cur == 1:
-				output.innerHTML = "50K";
-				monies.innerHTML = "$9.00";
-				break;
-			case cur == 2:
-				output.innerHTML = "100K";
-				monies.innerHTML = "$12.00";
-				break;
-			case cur == 3:
-				output.innerHTML = "500K";
-				monies.innerHTML = "$18.00";
-				break;
-			case cur == 4:
-				output.innerHTML = "1M";
-				monies.innerHTML = "$27.00";
-				break;
 
-		}
-	}
-// output.innerHTML = this.value;
+
+
+function main() {
+
+	checkbox.addEventListener('change', () => change_values(checkbox.checked, slider.value));
+	slider.addEventListener("input", () => change_values(checkbox.checked, slider.value));
 }
 
 
+main();
 
 
 
 
 
-// output.innerHTML = slider.value;
+
 
 
 
